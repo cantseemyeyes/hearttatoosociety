bafd899be9e23929b1488dcefc659c8b3c431911
cantseemyeyes <jackyyet@usc.edu>
2021-06-09 13:59:02 -0700

    full revamp

--- scripts/menu.js
+++ scripts/menu.js
@@ -38,7 +38,43 @@ More on javascript in a future lesson
 	$('.popupCloseButton').click(function(){
 		$('.hover_bkgr_fricc').hide();
 	});
-	
-	
+
 })(document);
 
+
+
+
+
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var stickyNavbar = document.getElementById("sticky_navbar");
+var regNavBar = document.getElementsByClassName("reg_navbar")[0];
+var regNavBarPhoto = document.getElementsByClassName("reg_navbar")[1];
+
+// Get the offset position of the navbar
+var sticky = stickyNavbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+	if(window.innerWidth > 700){
+		if (window.pageYOffset > sticky) {
+			regNavBar.style.visibility = "hidden";
+			regNavBarPhoto.style.visibility = "hidden";
+			stickyNavbar.style.display = "flex";
+			stickyNavbar.classList.add("sticky");
+		} else {
+			regNavBar.style.visibility = "visible";
+			regNavBarPhoto.style.visibility = "visible";
+			stickyNavbar.style.display = "none";
+			stickyNavbar.classList.remove("sticky");
+		}
+	}
+	else {
+		regNavBar.style.visibility = "visible";
+		regNavBarPhoto.style.visibility = "visible";
+		stickyNavbar.style.display = "none";
+		stickyNavbar.classList.remove("sticky");
+	}
+}
